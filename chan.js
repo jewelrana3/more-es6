0d204a1ceda50e369a22cb2a9c5b41812add9521
jewelrana3 <juyelrana7752@gmail.com>
2023-02-21 09:56:17 +0600

    input

---
+++ chan.js
@@ -0,0 +1,22 @@
+const data = {
+    count:22222,
+   data1 : [
+    {name: 'juyr',address:'bajde',job:'devoloper'},
+   {name: 'bai',address:'bajde',job:'leader'},
+]
+}
+// console.log(data.data1[1].job)
+
+const user = {
+    id:101,
+    file:{
+        first:{
+            name:'juyel rana boro vai',
+            manik:'juyel rana leader vai',
+            mama:'juyel rana choto vai',
+        },
+        address:'bademaju malitha para',
+        city:'alomdanga',
+}
+}
+console.log(user.file.first.manik)
